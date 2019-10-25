61653f1fd38dec99d4327d0f479b3ab5f41a7bc6
l4rzy <l4.foss@gmail.com>
2019-10-25 08:36:12 +0700

    indexing write out data

--- algo.py
+++ algo.py
@@ -1,13 +1,13 @@
-import pickle
+import pickle, os
 
 class TrieNode(object):
-    def __init__(self, value = 0):
+    def __init__(self, value = -1):
         self.children : Dict[str, TrieNode] = {}
         self.value = value
 
     @property
     def is_leaf(self):
-        if self.value > 0:
+        if self.value >= 0:
             return True
         return False
 
@@ -39,7 +39,7 @@ class TrieNode(object):
 
     @staticmethod
     def traverse_leaves(node , buf: str, result = []):
-        if node.value != 0:
+        if node.is_leaf:
             result.append((buf, node.value))
         else:
             for key in node.children:
@@ -60,48 +60,68 @@ class TrieNode(object):
 
 
 class DictionaryData(object):
-    def __init__(self, datafile='', idxfile=''):
-        self.trie = TrieNode()
-        try:
-            self.data = open(datafile, 'r')
-        except Exception as e:
-            print(f':: error opening datafile {e}')
+    def __init__(self, idxfile='', datafile='', inputfile=''):
+        self.idxname = idxfile
+        self.dataname = datafile
+        self.inputfile = inputfile
 
+        self.trie = None
+        self.data = None
+
+        ## open index file if exists
         try:
-            with open(idxfile, 'rb') as f:
+            with open(idxfile, 'rb+') as f:
                 self.trie = pickle.load(f)
                 print(":: index file loaded")
         except Exception as e:
-            print(":: index file not found, indexing ... ", end='')
-            self.do_indexing(datafile, idxfile)
+            print(f':: error opening index file \n\t{e}')
+
+        ## open data file to read meanings
+        try:
+            self.data = open(datafile, 'rb+')
+            print(':: data file loaded')
+        except Exception as e:
+            self.data = open(datafile, 'wb+')
+            print(f':: error opening datafile \n\t{e}')
+
+        ## do indexing if data doesn't exist
+        if self.data is None or self.trie is None:
+            print(':: doing indexing ...')
+            self.do_indexing()
+
 
     def write_trie(self, name):
         with open(name, 'wb') as f:
             pickle.dump(self.trie, f)
 
-    def write_meanings(self, word):
-        pass
+    def write_data(self, meaning):
+        data = bytearray(meaning, 'utf-8')
+        data.append(0x00)
+        offset = self.data.seek(0, os.SEEK_END)
+        self.data.write(data)
+        return offset
 
-    def do_indexing(self, datafile, idxfile):
-        entries = 0
-        f = open(datafile, 'r')
+    def do_indexing(self):
+        self.trie = TrieNode()
+        num = 0
+        try:
+            f = open(self.inputfile, 'r')
+        except Exception as e:
+            print(f':: error reading input file {e}')
 
-        end = False
-        while not end:
-            offset = f.tell()
+        while True:
             line = f.readline()
             if line == '' or line == '\n':
-                end = True
-                continue
+                break
 
             entry = line[:-1].split(':')
-            offset += len(entry[0]) + 1
+            offset = self.write_data(entry[1])
             self.trie.insert(entry[0], offset)
-            entries += 1
+            num += 1
 
         f.close()
-        self.write_trie(idxfile)
-        print(f'done with {entries} entries')
+        self.write_trie(self.idxname)
+        print(f'done with {num} entries')
 
     def search(self, keyword: str):
         result = self.trie.search(keyword)
@@ -111,14 +131,21 @@ class DictionaryData(object):
 
     def read_meaning(self, offset):
         self.data.seek(offset)
-        return self.data.readline().strip()
+
+        result = bytearray()
+        while True:
+            b = self.data.read(1)
+            if b == b'\x00':
+                break
+            result += (b)
+
+        return result.decode('utf-8')
 
 if __name__ == "__main__":
-    d = DictionaryData('dict.txt', 'dict.idx')
+    d = DictionaryData('dict.idx', 'dict.data', 'dict.txt')
     while True:
         q = input('> ')
         if q == 'END':
             exit()
         else:
             d.search(q)
-
